6ae28f487349fc84683476b268cba1e7d1f6ab62
MOGAU GODFREY NGWATLE <ngwatlegodfrey@gmail.com>
2022-10-03 15:22:50 +0200

    made changes to the java program files based on the comments received from mentor

--- ArrayList_Task/src/Main.java
+++ ArrayList_Task/src/Main.java
@@ -3,13 +3,36 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void displayArrayList(ArrayList<String> numbers){
+    public static void replaceOldToNewValue(ArrayList<String> numbers, String oldElement, String newValue) {
+
+        try {
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i).equalsIgnoreCase(oldElement)) {
+                    System.out.print("\nEnter a new value: ");
+                    Scanner input = new Scanner(System.in);
+                    newValue = input.nextLine();
+                    numbers.set(i, newValue);
+                }
+            }
+            System.out.println("\nThe new ArrayList after adding a new value");
+            System.out.println(numbers + " ");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> numbers = new ArrayList<>();
+        String oldElement = "";
+        String newValue = "";
 
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter your favorite numbers and type 'quit' to stop: ");
 
-        while(in.hasNextInt()){
+        while (in.hasNextInt()) {
             numbers.add(in.nextLine());
         }
         System.out.println("\nArrayList before replacing an element: ");
@@ -17,28 +40,11 @@ public class Main {
 
         System.out.print("\n\nEnter an element to remove from the ArrayList: ");
         Scanner accept = new Scanner(System.in);
-        String removeElement = accept.nextLine();
-
-        for (int i = 0; i < numbers.size(); i++){
-            if(numbers.get(i).equalsIgnoreCase(removeElement)){
-                System.out.print("\nEnter a new value: ");
-                Scanner newValue = new Scanner(System.in);
-                String replaceValue = newValue.nextLine();
-                numbers.set(i, replaceValue);
-            }
-        }
-        System.out.println("\nThe new ArrayList after adding a new value");
-        System.out.println(numbers + " ");
-
-    }
-
-    public static void main(String[] args) {
-
-        ArrayList<String> numbers = new ArrayList<>();
+        oldElement = accept.nextLine();
 
         Main m = new Main();
 
-        m.displayArrayList(numbers);
+        m.replaceOldToNewValue(numbers, oldElement, newValue);
 
     }
 }
--- LinkedList_Task/src/Main.java
+++ LinkedList_Task/src/Main.java
@@ -3,62 +3,62 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void receiveTwoCollection(LinkedList<String> linkedList1, LinkedList<String> linkedList2){
+    public static void mergeTwoCollection(LinkedList<String> firstCollection, LinkedList<String> secondCollection,
+            String elemetValue) {
 
-        // adding the first linked list
-        linkedList1.add("1");
-        linkedList1.add("2");
-        linkedList1.add("3");
-        linkedList1.add("4");
-        linkedList1.add("5");
+        try {
+            // a boolean to check first collection contains an input elemetValue
+            boolean checkContains = firstCollection.contains(elemetValue);
+            // declare an index to get the index of the input elemetValue
+            int index = firstCollection.indexOf(elemetValue);
+            index++;
 
-        // adding the second linked list
-        linkedList2.add("6");
-        linkedList2.add("7");
-        linkedList2.add("8");
-        linkedList2.add("9");
+            // if condition the check if the input value exists in the first collection
+            // if it exists then we insert the second collection from the input value
+            if (checkContains) {
+                LinkedList<String> mergeCollection = new LinkedList<>();
 
-        System.out.print("First collection: ");
-        for (String list1 : linkedList1){
-            System.out.print(list1 + ",");
-        }
-
-        System.out.print("\nSecond collection: ");
-        for (String list2 : linkedList2){
-            System.out.print(list2 + ",");
-        }
-
-        System.out.print("\nEnter element value from the first collection: ");
-        Scanner input = new Scanner(System.in);
-        String value = input.nextLine();
-
-        // a boolean to check first collection contains an input value
-        boolean checkContains = linkedList1.contains(value);
-        // declare an index to get the index of the input value
-        int index = linkedList1.indexOf(value);
-        index++;
-
-        // if condition the check if the input value exists in the first collection
-        // if it exists then we insert the second collection from the input value
-        if (checkContains){
-            LinkedList<String> combineLists = new LinkedList<>();
-
-            combineLists.addAll(linkedList1);
-            combineLists.addAll(index, linkedList2);
-            System.out.println("Results: " + combineLists);
-        }else{
-            System.out.println("Value does not exist in the first collection");
+                mergeCollection.addAll(firstCollection);
+                mergeCollection.addAll(index, secondCollection);
+                System.out.println("Results: " + mergeCollection);
+            } else {
+                System.out.println("Value does not exist in the first collection");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
 
     }
 
     public static void main(String[] args) {
 
-        LinkedList<String> linkedList1 = new LinkedList<>();
-        LinkedList<String> linkedList2 = new LinkedList<>();
+        LinkedList<String> firstCollection = new LinkedList<>();
+        LinkedList<String> secondCollection = new LinkedList<>();
+        String value = "";
+
+        // adding the first collection
+        firstCollection.add("1");
+        firstCollection.add("2");
+        firstCollection.add("3");
+        firstCollection.add("4");
+        firstCollection.add("5");
+
+        // adding the second collection
+        secondCollection.add("6");
+        secondCollection.add("7");
+        secondCollection.add("8");
+        secondCollection.add("9");
+
+        // printing the first and second collections
+        System.out.println("First collection: " + firstCollection);
+        System.out.println("Second collection: " + secondCollection);
+
+        System.out.print("\nEnter a number from the first collection to insert second collection from the number: ");
+        Scanner input = new Scanner(System.in);
+        value = input.nextLine();
 
         Main m = new Main();
 
-        m.receiveTwoCollection(linkedList1, linkedList2);
+        m.mergeTwoCollection(firstCollection, secondCollection, value);
     }
 }
